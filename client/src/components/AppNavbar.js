d08d5d14507a1520449d91b9ae7addc880e2908d
aripan <aripan017@gmail.com>
2020-08-20 22:58:29 +0200

    Client Setup & Reactstrap

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+} from "reactstrap";
+
+class AppNavbar extends Component {
+  state = {
+    isOpen: false,
+
+    //! if we do not want to bind the method, then we can write the method as a function
+    //    this.toggle = this.toggle.bind(this);
+  };
+
+  // Customized method
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/aripan">Github</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
